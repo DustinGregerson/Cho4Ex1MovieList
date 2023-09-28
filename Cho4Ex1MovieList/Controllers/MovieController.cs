57e9daa6769fd09d53475eb40e640d662a9d043e
DustinGregerson <dustin.gregerson@southeasttech.edu>
2023-09-28 12:04:31 -0500

    index and edit html now work with the genre's table/genre's class and display the genre of each movie

--- Cho4Ex1MovieList/Controllers/MovieController.cs
+++ Cho4Ex1MovieList/Controllers/MovieController.cs
@@ -17,12 +17,14 @@ namespace Cho4Ex1MovieList.Controllers
         public IActionResult Add()
         {
             ViewBag.Action = "Add";
+            ViewBag.Genres = context.Genres.OrderBy(g => g.Name).ToList();
             return View("Edit", new Movie());
         }
         [HttpGet]
         public IActionResult Edit(int id)
         {
             ViewBag.Action = "Edit";
+            ViewBag.Genres = context.Genres.OrderBy(g => g.Name).ToList();
             var movie = context.Movies.Find(id);
             return View(movie);
         }
@@ -41,6 +43,7 @@ namespace Cho4Ex1MovieList.Controllers
             else
             {
                 ViewBag.Action = (movie.MovieId == 0) ? "Add" : "Edit";
+                ViewBag.Genres = context.Genres.OrderBy(g => g.Name).ToList();
                 return View(movie);
             }
         }
