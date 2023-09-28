3655ae0ca1f9952b3654a46386eb810a39920e1d
DustinGregerson <dustin.gregerson@southeasttech.edu>
2023-09-28 09:00:54 -0500

    Changed the home controller to return the moviescontexts movie list to the index page. changed the index to pull the movies context from the home controller changed the index by adding a table that displays the movie name, year, and Rating for each context changed the link for Bootstrap in the layout

--- Cho4Ex1MovieList/Controllers/HomeController.cs
+++ Cho4Ex1MovieList/Controllers/HomeController.cs
@@ -1,13 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Cho4Ex1MovieList.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Cho4Ex1MovieList.Controllers
 {
     public class HomeController : Controller
     {
+        private MovieContext context { get; set; }
+
+        public HomeController(MovieContext ctx)
+        {
+            context = ctx;
+        }
 
         public IActionResult Index()
         {
-            return View();
+            var movies = context.Movies.OrderBy(m=>m.Name).ToList();
+            return View(movies);
         }
     }
 }
